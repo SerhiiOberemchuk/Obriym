203cf2ad5566f4922431a73dc92ce2673a2ed39e
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-06-24 19:42:32 +0200

    dynamic-sitemap

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,30 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import type { DocumentHead, RequestHandler } from "@builder.io/qwik-city";
 import style from "./styles.css?inline";
+import { routes } from "@qwik-city-plan";
+import { createSitemap } from "./create-sitemap";
+
+export const onGet: RequestHandler = (ev) => {
+  const siteRoutes = routes
+    .map(([route]) => route as string)
+    .filter(route => route !== "/");  // Exclude the '/' route
+ 
+  const sitemap = createSitemap([
+    { loc: "/", priority: 1 },  // Manually include the root route
+    ...siteRoutes.map((route) => ({
+      loc: route,
+      priority: 0.9,  // Default priority, adjust as needed
+    })),
+  ]);
+ 
+  const response = new Response(sitemap, {
+    status: 200,
+    headers: { "Content-Type": "text/xml" },
+  });
+ 
+  ev.send(response);
+};
+
 
 export default component$(() => {
   useStylesScoped$(style)
