d821aa539e849bb934ee3aec7fc01983f7d507df
GannaKov <zlatta2000@gmail.com>
2025-07-07 23:47:05 +0200

    added images and icons

--- src/components/sections/section-contact/content-contact/ContentContact.tsx
+++ src/components/sections/section-contact/content-contact/ContentContact.tsx
@@ -1,5 +1,11 @@
 import { component$, useStylesScoped$ } from "@qwik.dev/core";
 import styles from "./styles_content.css?inline";
+// import IconEmail1 from "/public/icons/icon_email1?w=24&h24&jsx";
+// import IconEmail from "/public/icons/icon_email.svg?w=24&h24&jsx";
+import IconTelegram from "/public/icons/icon_telegram.svg?w=24&h24&jsx";
+import OberemImg from "../../../../assets/images/oberem-image.png?jsx";
+import IconSchedule from "/public/icons/icon_schedule.svg?w=64&h64&jsx";
+import LinkEmail from "~/components/common/link-email/LinkEmail";
 
 export default component$(() => {
   useStylesScoped$(styles);
@@ -19,12 +25,13 @@ export default component$(() => {
             Let&apos;s start the conversation and explore how we can bring your vision to life.
           </p>
           <div class="cc_text_soc_media_box">
-            <div class="cc_soc_media_row">
-              <p>icon</p>
+            {/* <div class="cc_soc_media_row">
+              <IconEmail class="cc_email_icon" />
               <p class="btn_header">info@obriym.com</p>
-            </div>
+            </div> */}
+            <LinkEmail place="main" />
             <div class="cc_soc_media_row">
-              <p>icon</p>
+              <IconTelegram />
               <p class="btn_header">@obriym</p>
             </div>
           </div>
@@ -34,7 +41,9 @@ export default component$(() => {
       <div class="cc_calendly_box">
         <div class="cc_calendly__foto_box_blue">
           <div class="cc_calendly__foto_box_white">
-            <div class="cc_calendly__foto">foto</div>
+            <div class="cc_calendly__foto">
+              <OberemImg />
+            </div>
           </div>
         </div>
         <div class="cc_calendly_text_box">
@@ -43,7 +52,8 @@ export default component$(() => {
             {/* Change to grey color .btn_body*/}
             <p class="btn_body">Founder & CEO</p>
           </div>
-          <div class="cc_calendly_text_icon">icon</div>
+
+          <IconSchedule class="cc_calendly_text_icon" />
         </div>
       </div>
     </div>
