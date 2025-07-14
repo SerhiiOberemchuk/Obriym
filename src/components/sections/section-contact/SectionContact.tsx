e483ca9b34976f22960db347bf0b13b787bbbc55
GannaKov <zlatta2000@gmail.com>
2025-07-14 14:07:07 +0200

    Added translate for contact sections

--- src/components/sections/section-contact/SectionContact.tsx
+++ src/components/sections/section-contact/SectionContact.tsx
@@ -1,17 +1,21 @@
 import { component$, useStylesScoped$ } from "@qwik.dev/core";
+import { inlineTranslate } from "qwik-speak";
 import ContentContact from "./content-contact/ContentContact";
 import InputsContact from "./inputs-contact/InputsContact";
+
 import styles from "./styles_contact.css?inline";
 import IconGreen from "~/assets/images/green.png?w=100&h100&jsx";
 
 export default component$(() => {
+  const t = inlineTranslate();
   useStylesScoped$(styles);
   return (
     <section class="c_section">
       <div class="container ">
         <div class="c_box_title">
           <IconGreen class="c_title_icon" />
-          <h2 class="H3_uppercase grey_dark">get a contact</h2>
+
+          <h2 class="H3_uppercase grey_dark">{t("home.contact-section.title@@get a contact")}</h2>
         </div>
 
         <div class="c_container">
