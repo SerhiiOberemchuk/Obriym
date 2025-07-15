6c3dc659de7ab7a070bc8792908574a7affa2f68
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-07-15 12:22:05 +0200

    sel/lang/with popover

--- src/components/common/top-menu/TopMenu.tsx
+++ src/components/common/top-menu/TopMenu.tsx
@@ -3,9 +3,10 @@ import { ChangeLocale } from "./change-locale/change-locale";
 import "./tm-styles.css";
 import LetsWork from "./lets-work/LetsWork";
 
-export default component$(() => {
+type Props = { place: "mob-menu" | "header" };
+export default component$<Props>(({ place }) => {
   return (
-    <div class="tm_container">
+    <div class="tm_container" data-place={place}>
       <ChangeLocale />
       <LetsWork />
     </div>
