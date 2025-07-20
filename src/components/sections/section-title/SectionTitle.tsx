2de948ce8fe88053ac37a0770cfc50a35568720d
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-07-20 13:47:57 +0200

    title section

--- src/components/sections/section-title/SectionTitle.tsx
+++ src/components/sections/section-title/SectionTitle.tsx
@@ -8,7 +8,7 @@ export default component$(() => {
       <div class="container">
         <h1 class="H2_light black title">
           <span>Complete</span> <span class="H1_extra_light grey_dark">digital</span>
-          <img src="/images/green.png" width={124} height={124} alt="dfd" />
+          <img src="/images/green.png" class="spring_img" width={124} height={124} alt="dfd" />
           <span class="H1_extra_light grey_dark">products.</span>
           <img
             src="/images/hero_slides.png"
@@ -17,17 +17,18 @@ export default component$(() => {
             height={124}
             class="notebook tablet"
           />
-          <span>From</span>
-          <span>concept to launch</span>
-          <canvas></canvas>
-          <img
-            src="/images/hero_slides.png"
-            alt="desc"
-            width={233}
-            height={124}
-            class="notebook mobile"
-          />
+          <span class="from_sp1">From</span>
+          <span class="text_center">
+            <span class="from_sp2">From</span> concept to launch
+          </span>
         </h1>
+        <img
+          src="/images/hero_slides.png"
+          alt="desc"
+          width={233}
+          height={124}
+          class="notebook mobile"
+        />
       </div>
     </section>
   );
