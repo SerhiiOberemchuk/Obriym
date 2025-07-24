20e8985874605c1c15c0ce0f82bc45673e27dabc
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-07-24 22:27:22 +0200

    Update SectionTitle.tsx

--- src/components/sections/section-title/SectionTitle.tsx
+++ src/components/sections/section-title/SectionTitle.tsx
@@ -1,10 +1,10 @@
 import { component$, useStylesScoped$ } from "@qwik.dev/core";
 import styles from "./st-styles.css?inline";
 import { inlineTranslate } from "qwik-speak";
-// import ImgGreen from "~/assets/images/green.png?w124&h124&jsx";
+import ImgGreen from "~/assets/images/green.png?w124&h124&jsx";
 import ImgHeroSl from "~/assets/images/hero_slides.png?w233&h124&jsx";
 // import { useResponsive } from "~/hooks/useResponsive";
-import { QModel } from "~/integrations/react/model/ModelGLB";
+// import { QModel } from "~/integrations/react/model/ModelGLB";
 
 export default component$(() => {
   useStylesScoped$(styles);
@@ -19,8 +19,8 @@ export default component$(() => {
         <h1 class="H2_light black title">
           <span>{t("home.stitle.1span@@Complete")}</span>
           <span class="H1_extra_light grey_dark">{t("home.stitle.2span@@digital")}</span>
-          {/* <ImgGreen class="spring_img" alt="dfd" /> */}
-          <QModel model="spring" width={75} height={75} />
+          <ImgGreen class="spring_img" alt="dfd" />
+          {/* <QModel model="spring" width={75} height={75} /> */}
           <span class="H1_extra_light grey_dark">{t("home.stitle.3span@@products")}.</span>
           <ImgHeroSl class="notebook tablet" alt="desc" />
 
