80b6ba6dd4936bfd258f83995d1a665c70599d4f
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-07-26 21:54:26 +0200

    fix page speed errors

--- src/types/social-links.type.ts
+++ src/types/social-links.type.ts
@@ -4,6 +4,7 @@ type SocialLinks = {
   network: SocialNetwroks;
   link: string;
   iconLink: string;
+  ariaLabel?: string;
 };
 
 export const socialLinks: SocialLinks[] = [
@@ -11,16 +12,19 @@ export const socialLinks: SocialLinks[] = [
     network: "facebook",
     link: "https://www.facebook.com/obriym",
     iconLink: "/icons/icon-facebook.svg",
+    ariaLabel: "Link to facebook account",
   },
   {
     network: "linkedIn",
     link: "https://www.linkedin.com/company/obriym/",
     iconLink: "/icons/icon-linkedIn.svg",
+    ariaLabel: "Link to linkedin account",
   },
   //   { network: "threads", link: "https://www.threads.com/@obriym_web_agency/" },
   {
     network: "instagram",
     link: "https://www.instagram.com/obriym_web_agency/",
     iconLink: "/icons/icon-instagram.svg",
+    ariaLabel: "Link to instagram account",
   },
 ];
