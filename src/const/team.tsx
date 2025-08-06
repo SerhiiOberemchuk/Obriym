24d8a4c05342bd197667e78ba5cbaa362274a42e
GannaKov <zlatta2000@gmail.com>
2025-08-06 17:00:43 +0200

    person 4, 2 description

--- src/const/team.tsx
+++ src/const/team.tsx
@@ -1,8 +1,9 @@
 import { inlineTranslate } from "qwik-speak";
 import { TeamMemberType, ImageKey } from "~/types/team-member.type";
-import Person1 from "~/assets/images/oberemchuk.png?w=880&h=1192&jsx";
-import Person2 from "~/assets/images/person2.png?w=880&h=1192&jsx";
-import Person3 from "~/assets/images/person3.png?w=880&h=1192&jsx";
+import Oberemchuk from "~/assets/images/oberemchuk.png?w=880&h=1192&jsx";
+import Kovchyk from "~/assets/images/ganna_kovchyk.png?w=880&h=1192&jsx";
+import Nazarenko from "~/assets/images/olga_nazarenko.png?w=880&h=1192&jsx";
+import Kiriiaka from "~/assets/images/svitlana_kiriiaka.png?w=880&h=1192&jsx";
 import type { JSX } from "@qwik.dev/core";
 
 export const TEAM_MEMBERS: TeamMemberType[] = [
@@ -12,29 +13,47 @@ export const TEAM_MEMBERS: TeamMemberType[] = [
     slug: "serhii_oberemchuk",
     role: "Founder & CEO",
     imageKey: "person1",
-    description:
-      " Serhii is a strategic leader with a clear vision for how design, technology, and business intersect. As the CEO and co-founder of the agency, he brings over a decade of experience in building teams, managing complex projects, and scaling digital products. Before launching the agency. Serhii led digital transformation initiatives across multiple industries, where he identified the need for a more agile, design-driven approach to business growth.",
+    description1:
+      "Serhii is a strategic leader with a clear vision for how design, technology, and business intersect. As the CEO and co-founder of the agency, he brings over a decade of experience in building teams, managing complex projects, and scaling digital products. ",
+    description2:
+      "Before launching the agency, Serhii led digital transformation initiatives across multiple industries, where he identified the need for a more agile, design-driven approach to business growth.",
     linkedin: "https://www.linkedin.com/in/serhii-oberemchuk/",
   },
   {
     id: 2,
-    name: "Person 2",
-    slug: "person_2",
-    role: "Designer",
+    name: "Ganna Kovchyk",
+    slug: "ganna_kovchyk",
+    role: "Full stack Developer",
     imageKey: "person2",
-    description:
-      " Person2 is a strategic leader with a clear vision for how design, technology, and business intersect. As the CEO and co-founder of the agency, he brings over a decade of experience in building teams, managing complex projects, and scaling digital products. Before launching the agency. Serhii led digital transformation initiatives across multiple industries, where he identified the need for a more agile, design-driven approach to business growth.",
-    linkedin: "https://www.linkedin.com/in/serhii-oberemchuk/",
+    description1:
+      "Ganna is a Full Stack Developer who works across both front-end and back-end, focusing on functionality, performance, and reliability. She is not afraid to implement unconventional or challenging solutions when the project calls for it. Always polishing her work and aiming for tangible results, Ganna brings a thoughtful, detail-oriented mindset to every task.",
+    description2:
+      "Outside of work, she enjoys surfing, spending time in nature, and values freedom — in code, in life, and in thinking.",
+    linkedin: "https://www.linkedin.com/in/ganna-kovchyk/",
   },
   {
     id: 3,
-    name: "Person 3",
-    slug: "person_3",
-    role: "Product Manager",
+    name: "Olga Nazarenko",
+    slug: "olga_nazarenko",
+    role: "UX UI Designer",
     imageKey: "person3",
-    description:
-      " Person3 is a strategic leader with a clear vision for how design, technology, and business intersect. As the CEO and co-founder of the agency, he brings over a decade of experience in building teams, managing complex projects, and scaling digital products. Before launching the agency. Serhii led digital transformation initiatives across multiple industries, where he identified the need for a more agile, design-driven approach to business growth.",
-    linkedin: "https://www.linkedin.com/in/serhii-oberemchuk/",
+    description1:
+      "Olga is a UX UI designer who values structure and system thinking, always focusing on creating products that work seamlessly and solve real user needs. She’s open to modern design trends and innovative solutions but believes they should be balanced with thoughtful, purposeful design, not just trendy visuals.",
+    description2:
+      "Outside of work, she enjoys exploring new ideas and staying curious about how design and technology can come together to improve everyday life.",
+    linkedin: "https://www.linkedin.com/in/olga-nazarenko/",
+  },
+  {
+    id: 4,
+    name: "Svitlana Kiriiaka",
+    slug: "svitlana_kiriiaka",
+    role: "UX UI Designer",
+    imageKey: "person4",
+    description1:
+      "Svitlana is a UX UI designer who values structure and system thinking, always focusing on creating products that work seamlessly and solve real user needs. She’s open to modern design trends and innovative solutions but believes they should be balanced with thoughtful, purposeful design, not just trendy visuals.",
+    description2:
+      "Outside of work, she enjoys exploring new ideas and staying curious about how design and technology can come together to improve everyday life.",
+    linkedin: "https://www.linkedin.com/in/svitlana-kiriiaka/",
   },
 ];
 
@@ -42,7 +61,7 @@ export const imageMap: Record<ImageKey, () => JSX.Element> = {
   person1: () => {
     const t = inlineTranslate();
     return (
-      <Person1
+      <Oberemchuk
         class="slide-image"
         draggable={false}
         role="img"
@@ -55,12 +74,12 @@ export const imageMap: Record<ImageKey, () => JSX.Element> = {
   person2: () => {
     const t = inlineTranslate();
     return (
-      <Person2
+      <Kovchyk
         class="slide-image"
         draggable={false}
         role="img"
-        aria-label={t("team.member.person_2.image_alt@@Photo of {{name}}", {
-          name: t("team.member.person_2.name@@Person 2"),
+        aria-label={t("team.member.ganna_kovchyk.image_alt@@Photo of {{name}}", {
+          name: t("team.member.ganna_kovchyk.name@@Ganna Kovchyk"),
         })}
       />
     );
@@ -68,30 +87,27 @@ export const imageMap: Record<ImageKey, () => JSX.Element> = {
   person3: () => {
     const t = inlineTranslate();
     return (
-      <Person3
+      <Nazarenko
         class="slide-image"
         draggable={false}
         role="img"
-        aria-label={t("team.member.person_3.image_alt@@Photo of {{name}}", {
-          name: t("team.member.person_3.name@@Person 3"),
+        aria-label={t("team.member.olga_nazarenko.image_alt@@Photo of {{name}}", {
+          name: t("team.member.olga_nazarenko.name@@Olga Nazarenko"),
+        })}
+      />
+    );
+  },
+  person4: () => {
+    const t = inlineTranslate();
+    return (
+      <Kiriiaka
+        class="slide-image"
+        draggable={false}
+        role="img"
+        aria-label={t("team.member.svitlana_kiriiaka.image_alt@@Photo of {{name}}", {
+          name: t("team.member.svitlana_kiriiaka.name@@Svitlana Kiriiaka"),
         })}
       />
     );
   },
 };
-// export const imageMap: Record<ImageKey, () => JSX.Element> = {
-//   person1: () => (
-//     <Person1
-//       class="slide-image"
-//       draggable={false}
-//       role="img"
-//       aria-label="Photo of Serhii Oberemchuk"
-//     />
-//   ),
-//   person2: () => (
-//     <Person2 class="slide-image" draggable={false} role="img" aria-label="Photo of Person 2" />
-//   ),
-//   person3: () => (
-//     <Person3 class="slide-image" draggable={false} role="img" aria-label="Photo of Person 3" />
-//   ),
-// };
