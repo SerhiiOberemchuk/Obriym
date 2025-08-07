cae61ab6835457e789d5f2cf8f1867a4efc877bf
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-08-07 12:17:57 +0200

    Update useFormAction.ts

--- src/utils/useFormAction.ts
+++ src/utils/useFormAction.ts
@@ -2,26 +2,24 @@ import { formAction$, valiForm$ } from "@modular-forms/qwik";
 import { ContactSchema } from "~/schemas/contactSchema";
 import { ContactForm, ContactFormResponse } from "~/types/contact-form.type";
 import { sendEmail } from "~/utils/sendEmail";
-import { getEmailHtmlToUs } from "./emailTemplates"; //, getEmailHtmlToUser
+import { getEmailHtmlToUs, getEmailHtmlToUser } from "./emailTemplates";
 
 export const useFormAction = formAction$<ContactForm, ContactFormResponse>(
   async (values, requestEvent) => {
-    // Runs on server
-
     const { services, budget, name, email, message } = values;
 
     const resendApiKey = requestEvent.env.get("RESEND_API_KEY");
-    const emailReceiver = requestEvent.env.get("EMAIL_RECEIVER");
+    const myDomainEmail = requestEvent.env.get("EMAIL_RECEIVER");
 
     if (!resendApiKey) throw new Error("RESEND_API_KEY is not defined");
-    if (!emailReceiver) throw new Error("EMAIL_RECEIVER is not defined");
+    if (!myDomainEmail) throw new Error("EMAIL_RECEIVER is not defined");
 
     const emailHtmlToUs = getEmailHtmlToUs({ name, email, services, budget, message });
-    //const emailHtmlToUser = getEmailHtmlToUser(name);
+    const emailHtmlToUser = getEmailHtmlToUser(name);
     try {
       const resultToUs = await sendEmail(resendApiKey, {
-        from: "Acme <onboarding@resend.dev>",
-        to: emailReceiver, //"info@obriym.com",
+        from: email,
+        to: myDomainEmail,
         subject: "New contact form submission",
         html: emailHtmlToUs,
       });
@@ -32,19 +30,16 @@ export const useFormAction = formAction$<ContactForm, ContactFormResponse>(
           message: resultToUs.error.message || "Error sending email to us.",
         };
       }
-      // WE NEED IT TO SEND EMAIL TO USER
-      // Trying to send an email to a user, but not waiting for an error
-      // await sendEmail(resendApiKey, {
-      //   from: "Acme <onboarding@resend.dev>", //"info@obriym.com",
-      //   to: email,
-      //   subject: "Thank you for reaching out!",
-      //   html: emailHtmlToUser,
-      // }).catch(err => {
-      //   console.error("Failed to send thank you email to user:", err);
-      //   // The error is logged, but does not affect the response to the client for popover
-      // });
 
-      //We return a successful response to the client, because email has been sent to us
+      await sendEmail(resendApiKey, {
+        from: myDomainEmail,
+        to: email,
+        subject: "Thank you for reaching out!",
+        html: emailHtmlToUser,
+      }).catch(err => {
+        console.error("Failed to send thank you email to user:", err);
+      });
+
       return {
         status: "success",
         message: "Your message was sent successfully!",
