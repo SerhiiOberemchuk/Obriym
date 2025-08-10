1d1157798e9efef19f262760342927084860cdc5
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-08-11 00:07:38 +0200

    Revert "Fix/carousel"

--- src/components/lets-work/LetsWork.tsx
+++ src/components/lets-work/LetsWork.tsx
@@ -1,34 +1,17 @@
-import { $, component$, createContextId, Signal, useContext, useStyles$ } from "@qwik.dev/core";
+import { component$, useStyles$ } from "@qwik.dev/core";
 import styles from "./lw-styles.css?inline";
 import { inlineTranslate } from "qwik-speak";
 import { Modal } from "@qwik-ui/headless";
 import ContactFormComponent from "../common/contact-form/ContactFormComponent";
-import { MobileMenuContext } from "../mobile-menu/MobileMenu";
-
-export const ModalLetsWork = createContextId<Signal<boolean>>("modal-lets-workf");
 
 export default component$(({ place }: { place: "mob-menu" | "header" }) => {
   const t = inlineTranslate();
-
   useStyles$(styles);
-  const { toggleMenu } = useContext(MobileMenuContext);
-  const contextModal = useContext(ModalLetsWork);
-  const handleLetsWorkButton = $(() => {
-    contextModal.value = !contextModal.value;
-    if (contextModal.value) {
-      toggleMenu();
-    }
-  });
   return (
-    <Modal.Root data-place={place} class="lw_wrapper" bind:show={contextModal}>
-      <button
-        type="button"
-        data-place={place}
-        onClick$={handleLetsWorkButton}
-        class="btn_body lw_button"
-      >
+    <Modal.Root data-place={place} class="lw_wrapper">
+      <Modal.Trigger type="button" data-place={place} class="btn_body lw_button">
         {t("app.btnLetsWork@@Let’s work")}
-      </button>
+      </Modal.Trigger>
       <Modal.Panel class="lw_panel">
         <ContactFormComponent modal />
       </Modal.Panel>
