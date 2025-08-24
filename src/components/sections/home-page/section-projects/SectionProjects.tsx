f8b562ea40e1278e2f9cae9aed025f1e322fa71c
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-08-24 18:07:24 +0200

    Update SectionProjects.tsx

--- src/components/sections/home-page/section-projects/SectionProjects.tsx
+++ src/components/sections/home-page/section-projects/SectionProjects.tsx
@@ -109,13 +109,7 @@ const CarouselComponent = component$<PropsCarousel>(
                       <p class="sr-only" itemProp="description">
                         {description}
                       </p>
-                      <ul class="sr-only">
-                        {item.technologies.map((tech, ind) => (
-                          <li key={ind}>
-                            <h3>{tech}</h3>
-                          </li>
-                        ))}
-                      </ul>
+
                       <ul class="list_technologies">
                         {feautures.map((item, index) => (
                           <li key={index} class="item_features helper_text grey_dark">
