17fc79de977ffb027568191b48af7ee7cedd6fbb
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-10-16 17:16:08 +0200

    update jsonLd schema FAQ

--- src/components/pages/FaqPage.tsx
+++ src/components/pages/FaqPage.tsx
@@ -3,8 +3,28 @@ import { component$ } from "@qwik.dev/core";
 import SectionTitle from "../sections/faq-page/title-section/SectionTitle";
 import LinksTitle from "../sections/faq-page/links-section/LinksTitle";
 import QuestionSection from "../sections/faq-page/question-section/QuestionSection";
+import { inlineTranslate } from "qwik-speak";
+import { faqStructure, QA } from "../sections/faq-page/question-section/utils";
 
 export default component$(() => {
+  const t = inlineTranslate();
+
+  const items: QA[] = Object.values(faqStructure)
+    .flat()
+    .map(id => ({
+      id,
+      q: t(`faq.items.${id}.q`),
+      a: t(`faq.items.${id}.a`),
+    }));
+  const faqJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    mainEntity: items.map(i => ({
+      "@type": "Question",
+      name: i.q,
+      acceptedAnswer: { "@type": "Answer", text: i.a },
+    })),
+  };
   return (
     <>
       <SectionTitle />
@@ -12,6 +32,7 @@ export default component$(() => {
       <QuestionSection groupe="process" />
       <QuestionSection groupe="pricing_quality_seo" />
       <QuestionSection groupe="postlaunch_support" />
+      <script type="application/ld+json" dangerouslySetInnerHTML={JSON.stringify(faqJsonLd)} />
     </>
   );
 });
